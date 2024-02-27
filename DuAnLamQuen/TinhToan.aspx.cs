17b0415c67f3680229e164a9d110de7d544d70a4
LQCpaka <boyrovip1539@gmail.com>
2024-02-27 08:51:14 +0700

    27/2/2024 First Push

--- DuAnLamQuen/TinhToan.aspx.cs
+++ DuAnLamQuen/TinhToan.aspx.cs
@@ -7,11 +7,37 @@ using System.Web.UI.WebControls;
 
 namespace DuAnLamQuen
 {
-    public partial class TinhToan : System.Web.UI.Page
+    public partial class Hello : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void ThucHienPhepTinh(Func<int, int, int> operation)
+        {
+            int a = int.Parse(txtNhapSoMot.Text);
+            int b = int.Parse(txtNhapSoHai.Text);
+            txtKetQua.Text = operation(a, b).ToString();
+        }
+        protected void btnChia_Click(object sender, EventArgs e)
+        {
+            ThucHienPhepTinh((a, b) => a / b);
+        }
+
+        protected void btnNhan_Click(object sender, EventArgs e)
+        {
+            ThucHienPhepTinh((a, b) => a * b);
+        }
+
+        protected void btnTru_Click(object sender, EventArgs e)
+        {
+            ThucHienPhepTinh((a, b) => a - b);
+        }
+
+        protected void btnCong_Click(object sender, EventArgs e)
+        {
+            ThucHienPhepTinh((a, b) => a + b);
         }
     }
 }
